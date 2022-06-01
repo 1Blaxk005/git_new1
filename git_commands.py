fa1c53afb78549ab069c5599c9d14cc502445bcb
Amantur <amanturm@icloud.com>
2022-06-01 22:33:55 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,13 @@
+git init - .git
+git remote add name url
+git remote add name url
+git pull origin master
+git status
+git add
+git commit
+git branch
+git branch name branch
+git checkout name branh
+git push origin master
+git reset filename 
+
